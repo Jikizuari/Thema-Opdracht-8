32e76c3ccce57be60b0e0dda304a23e0e9327343
Davey Kropf <davey.kropf@gmail.com>
2013-06-18 15:10:24 +0200

    ...

--- essentials/header.php
+++ essentials/header.php
@@ -10,7 +10,7 @@
 	
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Droid+Serif' rel='stylesheet' type='text/css'>
-	<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
+	<link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
 </head>
 <body>
 	<header>
