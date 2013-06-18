e03bfa998810be0282bbdf2fee07a39dfec8b404
Davey Kropf <davey.kropf@gmail.com>
2013-06-18 14:55:41 +0200

    Added values My Account page

--- pages/myaccount.php
+++ pages/myaccount.php
@@ -11,6 +11,8 @@
 
 			try {
 				$output = $client->GetUserData(array('UserID'=>$_SESSION['user_id']));
+				$DateOfBirth = $output->DateOfBirth;
+				$DateOfBirth = str_replace("Z", "", $DateOfBirth);
 			} catch(Exception $e) {
 				require_once('essentials/usererror.php');
 				$u = new userError();
@@ -18,7 +20,14 @@
 				echo '<div class="errormessage" id="notification">'.$errorMessage.'</div>';
 			}
 		?>
-		<?php var_dump($output) ?>
+		<table>
+			<tr><td><span>Klantnummer</span> <?php echo $output->UserID ?></td><td><span>Woonplaats</span> <?php echo $output->City ?></td></tr>
+			<tr><td><span>Voornaam</span> <?php echo $output->Name ?></td><td><span>Geboortedatum</span> <?php echo $DateOfBirth ?></td></tr>
+			<tr><td><span>Achternaam</span> <?php echo $output->LastName ?></td><td><span>Telnummer</span> <?php echo $output->PhoneNumber ?></td></tr>
+			<tr><td><span>Straat</span> <?php echo $output->Street ." ". $output->HouseNumber ?></td><td><span>E-mailadres</span> <?php echo $output->{'E-mail'} ?></td></tr>
+			<tr><td><span>Postcode</span> <?php echo $output->ZipCode ?></td><td></td></tr>
+		</table>
+		<a class="right button" href="?page=editaccount">Edit gegevens</a> <a class="right button" href="?page=myholidays">Mijn boekingen</a>
 	</div>
 	<?PHP require_once("essentials/sidebar.php"); ?>
 </div>
