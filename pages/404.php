a4f8a860ddcc42864cb02c768a4e5e02c78a7ad6
Davey Kropf <davey.kropf@gmail.com>
2013-06-18 15:16:10 +0200

    404 page

--- pages/404.php
+++ pages/404.php
@@ -1,6 +1,8 @@
 <div class="right" id="main-content-right">
-<h1>404</h1>
-<p>Deze pagina is momenteel op vakantie!</p>
+	<div class="left" id="main-content-content">
+		<h1><strong>404</strong> - Pagina niet gevonden</h1>
+		<p>Deze pagina is momenteel op vakantie!</p>
+	</div>
 	<?PHP require_once("essentials/sidebar.php"); ?>
 </div>
 <div class="clear"></div>
