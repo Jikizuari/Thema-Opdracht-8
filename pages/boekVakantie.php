4e1ab00869dced6e240f33529b1063ff64743c8c
krukas <maikel_martens_1@hotmail.com>
2013-06-19 13:46:10 +0200

    boek vakantie changed

--- pages/boekVakantie.php
+++ pages/boekVakantie.php
@@ -1,4 +1,5 @@
 <div class="right" id="main-content-right">
+	<pre>
 <?PHP 
 if(isset($_SESSION['stap4']) && $_SESSION['stap4']){
 	$_SESSION['stap5'] = true;
@@ -6,132 +7,155 @@ if(isset($_SESSION['stap4']) && $_SESSION['stap4']){
 		$_SESSION['attr_id'] = $_GET['attr'];
 }
 
-if(!isset($_SESSION['user_id'])){ ?>
-	<h1>Gebruiker account</h1>
-	<p>Maak een <A HREF="index.php?page=register">account aan</a> of <a href="index.php?page=login">login</a>?</p>
-	</div><div class="clear"></div>
-<?PHP die(); }
+$vlucht = 0;
+$hotel 	= 0;
+$car 	= 0;
+$attr 	= 0;
 
-$vlucht = null;
-$room = null;
-$car = null;
-$attr = null;
-
-if(isset($_SESSION['stap5']) && $_SESSION['stap5']) {
-	//get vlucht
-	$client	= new SoapClient("http://tomcat.dkmedia.nl/flightservice/flightservice?wsdl");
-
-	$req 						= new stdClass();
-	$req->departureAirport 		= $_SESSION['vertrekhaven'];
-	$req->arrivalAirport 		= $_SESSION['bestemming'];
-	$req->departureDate 		= $_SESSION['vanDatum'];
-	$req->arrivalDate 			= $_SESSION['totDatum'];
-
-	try {
-		$result	= $client->searchFlight($req);
-		if(is_array($result->return)) {
-			foreach ($result->return as $v) {
-				if($v->flightId == $_SESSION['vlucht_id'])
-					$vlucht = $v; 
-			}
-		} else {
-			if($result->return->flightId == $_SESSION['vlucht_id'])
-					$vlucht = $result->return; 
-		}
-	} catch(Exception $e) {
-		//echo '<div class="errormessage" id="notification">'.$e->detail->fault->message.'</div>';
-		echo $e;
-	}
-
-	//get hotel/room
-	$client	= new SoapClient("http://tomcat.dkmedia.nl/hotelservice/hotelservice?wsdl");
-	$req 					= new stdClass();
-	$req->hotelId 			= $_SESSION['hotel_id'];
-	$req->numberOfPersons 	= $_SESSION['aantalPersonen'];
-	$req->fromDate 			= $_SESSION['vanDatum'];
-	$req->toDate 			= $_SESSION['totDatum'];
-
-	try {
-		$result	= $client->ShowAvailableRooms($req);
-		if(is_array($result->rooms->room)) {
-			foreach ($result->rooms->room as $r) {
-				if($r->id == $_SESSION['hotel_roomType'])
-					$room = $r;
-			}
-		} else {
-			if($result->rooms->room->id == $_SESSION['hotel_roomType'])
-					$room = $result->rooms->room;
-		}
-	} catch(Exception $e) {
-		//echo '<div class="errormessage" id="notification">'.$e->detail->fault->message.'</div>';
-		echo $e;
-	}
-
-	//get car
-	$client	= new SoapClient("http://iis.dkmedia.nl/TO8.VervoerService.svc?wsdl");
-	$req 					= new stdClass();
-	$req->numP 	= $_SESSION['aantalPersonen'];
-
-	try {
-		$result	= $client->GetAvailableCars($req);
-		if(isset($result->GetAvailableCarsResult->Car)) {
-			if(is_array($result->GetAvailableCarsResult->Car)) {
-				foreach ($result->GetAvailableCarsResult->Car as $value) {
-					if($value->carId == $_SESSION['auto_id'])
-						$car = $value;
-				}
-			} else {
-				if($result->GetAvailableCarsResult->Car == $_SESSION['auto_id'])
-					$car = $result->GetAvailableCarsResult->Car;
-			}
-		}
-	} catch(Exception $e) {
-		//echo '<div class="errormessage" id="notification">'.$e->detail->fault->message.'</div>';
-		echo $e;
-	}
-
-	//get attractie
-	class searchattractions{
-		var $holiday_end_dateField;
-		var $holiday_start_dateField;
-		var $placeField;
-		var $reachField;
-	}
-	class searchattraction {
-		var $searchattractions;
-		function __construct() {
-			$this->searchattractions = new searchattractions();
-		}
-	}
-	$client	= new SoapClient("http://iis.dkmedia.nl:85/WcfServiceLibrary1.Service1.svc?wsdl");
-	$req = new searchattraction();
-	$req->searchattractions->holiday_end_dateField		= $_SESSION['totDatum'];
-	$req->searchattractions->holiday_start_dateField		= $_SESSION['vanDatum'];
-	$req->searchattractions->placeField	= $_SESSION['bestemming'];
-	$req->searchattractions->reachField	= 50;
-	try {
-		$result	= $client->searchAttraction($req);
-		if(isset($result->searchAttractionResult->attraction)) {
-			if(is_array($result->searchAttractionResult->attraction)) {
-				foreach ($result->searchAttractionResult->attraction as $value) {
-					$attracties[] = $value;
-					if($value->idField == $_SESSION['attr_id'] )
-						$attr = $value;
-				}
-			} else {
-				if($result->searchAttractionResult->attraction->idField == $_SESSION['attr_id'] )
-						$attr = $result->searchAttractionResult->attraction;
-			}
-		}
-	} catch(Exception $e) {
-		//echo '<div class="errormessage" id="notification">'.$e->detail->fault->message.'</div>';
-		echo $e;
-	}
-
-
-} else {
+if( !(isset($_SESSION['stap5']) && $_SESSION['stap5']) ) {
 	redirect("vakantie");
 }
+//boek vlucht
+//######################################################################
+$client	= new SoapClient("http://tomcat.dkmedia.nl/flightservice/flightservice?wsdl");
+
+$req 						= new stdClass();
+$req->departureAirport 		= $_SESSION['vertrekhaven'];
+$req->arrivalAirport 		= $_SESSION['bestemming'];
+$req->departureDate 		= $_SESSION['vanDatum'];
+$req->arrivalDate 			= $_SESSION['totDatum'];
+// var_dump($client->__getFunctions());
+// var_dump($client->__getTypes());
+try {
+	//$result	= $client->searchFlight($req);
+	//$result = $client->bookFlight();
+
+} catch(Exception $e) { echo $e; }
+
+//boek hotel/room
+//######################################################################
+class room {
+	var $id;
+	var $numberOfrooms;
+}
+class rooms {
+	var $room;
+}
+class bookHotelRequest {
+	var $hotelId;
+	var $rooms;
+	var $numberOfPersons;
+	var $fromDate;
+	var $toDate;
+
+	function __construct() {
+		$this->rooms = new rooms();
+	}
+}
+$client	= new SoapClient("http://tomcat.dkmedia.nl/hotelservice/hotelservice?wsdl");
+$req 					= new bookHotelRequest();
+$req->hotelId 			= $_SESSION['hotel_id'];
+$req->numberOfPersons 	= $_SESSION['aantalPersonen'];
+$req->fromDate 			= $_SESSION['vanDatum'];
+$req->toDate 			= $_SESSION['totDatum'];
+$room					= new room();
+$room->id 				= $_SESSION['hotel_roomType'];
+$room->numberOfrooms	= $_SESSION['hotel_numberOfRooms'];
+$req->rooms->room[]		= $room;
+
+try {
+	// $result	= $client->BookHotel($req);
+	// if(isset($result->bookId))
+	// 	$hotel = $result->bookId;
+} catch(Exception $e) {	echo $e; }
+
+//boek car
+//######################################################################
+class rentCar {
+	var $name;
+	var $start;
+	var $end;
+	var $carid;
+}
+if(isset($_SESSION['auto_id'])) {
+	$client	= new SoapClient("http://iis.dkmedia.nl/TO8.VervoerService.svc?wsdl");
+	$req 		= new rentCar();
+	$req->name 	= $_SESSION['user_name']." ".$_SESSION['user_lastname'];
+	$req->start = $_SESSION['vanDatum'];
+	$reg->end 	= $_SESSION['totDatum'];
+	$reg->carid = $_SESSION['auto_id'];
+
+	try {
+		$result	= $client->RentCar($req);
+		if(isset($result->RentCarResult))
+			$car = $result->RentCarResult;
+	} catch(Exception $e) {	echo $e; }
+}
+
+	//boek attractie
+	//######################################################################
+class attractionbookingreq{
+	var $attractionidField ;
+	var $number_of_personsField ;
+	var $visit_dayField;
+}
+class bookAttraction {
+	var $attractionbookingreq;
+	function __construct() {
+		$this->attractionbookingreq = new attractionbookingreq();
+	}
+}
+if(isset($_SESSION['attr_id'])) {
+	$client	= new SoapClient("http://iis.dkmedia.nl:85/WcfServiceLibrary1.Service1.svc?wsdl");
+	$req = new bookAttraction();
+	$req->attractionbookingreq->attractionidField = $_SESSION['attr_id'];
+	$req->attractionbookingreq->number_of_personsField = $_SESSION['aantalPersonen'];
+	$req->attractionbookingreq->visit_dayField = $_SESSION['vanDatum'];
+	try {
+		$result	= $client->bookAttraction($req);
+		var_dump($result);
+	} catch(Exception $e) {	echo $e; }
+}
+
+//boek vakantie
+	//######################################################################
+	class Flights {
+		var $DepartureFlightID;
+		var $ReturnFlightID;
+	}
+	class BookHolidayRequest {
+		var $DepartureDate;
+		var $ReturnDate;
+		var $TotalPrice;
+		var $Flights;
+		var $HotelID;
+		var $TaxiID;
+		var $AttractionID;
+		var $UserID;
+		function __construct() {
+		$this->Flights = new Flights();
+		$this->AttractionID = array();
+	}
+}
+$client	= new SoapClient("http://tomcat.dkmedia.nl/tho8/userservice?wsdl");
+$req =  new BookHolidayRequest();
+$req->DepartureDate 		= $_SESSION['vanDatum'];
+$req->ReturnDate			= $_SESSION['totDatum'];
+$req->TotalPrice			= 200;
+$flights 					= new Flights();
+$flights->DepartureFlightID = 1;
+$flights->ReturnFlightID 	= 1;
+$req->Flights 				= $flights;
+$req->TaxiID				= $car;
+$req->HotelID 				= $hotel;
+$req->AttractionID[] 		= $attr;
+$req->UserID 				= $_SESSION['user_id'];
+
+try {
+	$result	= $client->BookHoliday($req);
+	var_dump($result);
+} catch(Exception $e) {	echo $e; }    
+
 ?>	
 <h1>Boek Vakantie</h1>
 <pre>
